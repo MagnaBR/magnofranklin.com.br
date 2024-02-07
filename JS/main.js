1bab89d7b7d3b17c2bd4ab1c01653ce7013b2060
MagnaBR <42671951+MagnaBR@users.noreply.github.com>
2024-02-07 17:32:48 -0300

    Initial

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,10 @@
 console.log('ANDREZÃO BORA FARMING 22!');
+
+
+
+(function(w, d){
+    let title = d.getElementById('title');
+    if (title) {
+        title += ' | Franklin Magno';
+    }
+})(window, document);
