2e7f967a360693ac40634756aa9430565bfc9c5a
MagnaBR <42671951+MagnaBR@users.noreply.github.com>
2024-02-19 02:48:59 -0300

    Initial

--- sw.js
+++ sw.js
@@ -13,10 +13,54 @@ self.addEventListener("install", (event) => {
     );
 });
 
+
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open(sw_version);
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, fallbackUrl }) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
 self.addEventListener("fetch", (event) => {
-    console.log(event);
-    event.respondWith(caches.match(event.request));
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      fallbackUrl: "/gallery/myLittleVader.jpg",
+    }),
+  );
 });
+
+
+
 
 const deleteCache = async (key) => {
     await caches.delete(key);
